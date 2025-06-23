d945da960caed3a6489c5a11859b09440050553b
SenneDrent <sennedrent@gmail.com>
2025-06-23 13:35:55 +0200

    vscode-with-exstentions and added nix language support extenstion

--- configuration.nix
+++ configuration.nix
@@ -99,12 +99,19 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  	vscode
-  	spotify
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
+  environment.systemPackages = with pkgs; 
+	  let
+	    vcsodeWithExtension = vscode-with-extensions.override {
+	      # When the extension is already available in the default extensions set.
+	      vscodeExtensions = with vscode-extensions; [
+		bbenoist.nix
+	      ];
+	    };
+	  in
+	  [
+	  	vcsodeWithExtension
+	  	spotify
+	  ];
   
    xdg.mime.enable = true;
    xdg.mime.defaultApplications = {
