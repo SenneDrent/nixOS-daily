575166ffcb9f5642052514165a32ca47b3b0fc69
SenneDrent <sennedrent@gmail.com>
2026-07-21 18:33:02 +0200

    steam fixes

--- nixosModules/programs/recreational/steam.nix
+++ nixosModules/programs/recreational/steam.nix
@@ -2,9 +2,13 @@
 
 {
     programs.steam = {
-        enable = true
-        emotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+        enable = true;
+        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
         dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
         localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    }
+        gamescopeSession.enable = true;
+    };
+    programs.gamemode.enable = true;    
+
+    environment.systemPackages = with pkgs; [ steam-run ];
 }
