4f3560bce2ff6c828e243b6e2501ce9ed6fb30e6
SenneDrent <sennedrent@gmail.com>
2026-07-21 18:42:12 +0200

    add discord to default apps

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -4,6 +4,7 @@
 	imports = [
 		./programs/coding/git.nix
 		./programs/coding/vscode.nix
+		./programs/recreational/discord.nix
 		./programs/recreational/spotify.nix
 		./programs/recreational/steam.nix
 		./programs/zen.nix
