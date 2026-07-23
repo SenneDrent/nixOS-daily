5a20912386b465ce844e338ac1429f739069f9d1
SenneDrent <sennedrent@gmail.com>
2026-07-23 15:29:23 +0200

    add freecad program

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -4,6 +4,7 @@
 	imports = [
 		./programs/coding/git.nix
 		./programs/coding/vscode.nix
+		./programs/tools/freedcad.nix
 		./programs/recreational/discord.nix
 		./programs/recreational/spotify.nix
 		./programs/recreational/steam.nix
